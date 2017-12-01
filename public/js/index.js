4935ad9ec374de54a6ee7c1269f4eb6d96198ee4
Akshay <patwardhanakshay8@gmail.com>
2017-12-01 17:08:16 +0530

    Update

--- public/js/index.js
+++ public/js/index.js
@@ -12,23 +12,26 @@ socket.on('disconnect', function () {
     console.log('Disconnected');
 });
 
-socket.on('newMessage', function(message) {
-    console.log('New message',message);
-    var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime} : ${message.text}`);
-
-    jQuery('#messages').append(li);
+socket.on('newMessage', function(message) {    
+    var formattedTime = moment(message.createdAt).format('h:mm a');    
+    var template = jQuery('#messageTemplate').html();
+    var html = Mustache.render(template, {
+        text : message.text,
+        from : message.from,
+        createdAt : formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from} ${formattedTime} : `);
-    a.attr('href', message.location_url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template = jQuery('#locationMessageTemplate').html();
+    var html = Mustache.render(template, {
+        locationURL : message.location_url,
+        from : message.from,
+        createdAt : formattedTime
+    });
+    jQuery('#messages').append(html);
 })
 
 socket.on('greetingAdmin', function(message) {
